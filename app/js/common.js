e33bb2e5234f733b58fafafaa7b6c643b802fb7e
Semen Milokhin <semen.milokhin@yandex.ru>
2019-02-21 21:32:46 +0300

    end of second day

--- app/js/common.js
+++ app/js/common.js
@@ -21,9 +21,7 @@ $(document).ready(function(){
 		list.animate({
 			left: "0%"			
 		},1000);
-		// hold.animate({
-		// 	width: "0%"
-		// },1000);
+		// hold.hide("slide", { direction: "left" }, 1000);
 	});
 	closeBtn.on('click', function(evt) {
 		evt.preventDefault();
@@ -34,4 +32,78 @@ $(document).ready(function(){
 		// 	width: "100%"
 		// },1000);
 	});
+
+	var controls = $('#section-about-slider-controls'),
+		lArr = controls.find('.slider__controls_left-arrow'),
+		rArr = controls.find('.slider__controls_right-arrow');
+
+	$('#section-about-slider').slick({
+		slidesToShow: '3',
+		appendArrows: controls,
+		prevArrow: lArr,
+		nextArrow: rArr
+	});
+
+
+
+
+	var newAds = $('.new-ads'),
+		newAdsItem = newAds.find('.new-ads__item');
+
+	newAdsItem.each(function(i,el){
+		var newAdsItemFavoritesBtn = $(el).find('.new-ads__favorites-btn'),
+			newAdsSliderWrapper    = $(el).find('.new-ads__slider-wrapper'),
+			newAdsImgSlider        = newAdsSliderWrapper.find('.new-ads__img-slider'),
+			newAdsSliderControls   = newAdsSliderWrapper.find('.new-ads__slider-controls'),
+			newAdsSliderLeftArr    = newAdsSliderControls.find('.new-ads__slider-controls_left-arrow'),
+			newAdsSliderRightArr   = newAdsSliderControls.find('.new-ads__slider-controls_right-arrow');
+
+		newAdsImgSlider.slick({
+			infinite: false,
+			appendArrows: newAdsSliderControls,
+			prevArrow: newAdsSliderLeftArr,
+			nextArrow: newAdsSliderRightArr
+		});
+
+		newAdsItemFavoritesBtn.on('click', function(evt){
+			evt.preventDefault();
+			$(el).toggleClass('in-favorites');
+		})
+	});
+
+
+
+	var menu = $('.menu'),
+		menuSections = menu.find('.menu__sections');
+
+	menuSections.each(function(i,el){
+		var btn = $(el).find('.menu__drop-down-btn'),
+			dropDown = $(el).find('.menu__drop-down'),
+			count = i;
+		dropDown.slideUp();
+		btn.on('click',function(evt){
+			evt.preventDefault();
+			menuSections.each(function(i,el){
+				if(i!==count) {
+					$(el).find('.menu__drop-down').slideUp();
+				}
+			})
+			dropDown.slideToggle();
+		})
+
+	});
+
+
+
+
+
+	// var mainSlider = $('#main-slider'),
+	// 	mainSliderControls = $('main-slider-controls'),
+	// 	mainSliderControlsLeftArrow = mainSliderControls.find('.main-slider-controls__item_left-arrow'),
+	// 	mainSliderControlsRightArrow = mainSliderControls.find('.main-slider-controls__item_right-arrow');
+	// mainSlider.slick({
+	// 	appendArrows: mainSliderControls,
+	// 	prevArrow: mainSliderControlsLeftArrow,
+	// 	nextArrow: mainSliderControlsRightArrow
+	// });
 });
